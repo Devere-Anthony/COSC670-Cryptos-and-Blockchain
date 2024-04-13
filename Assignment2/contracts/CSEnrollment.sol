b2881d49d5865f6600b81219a5b14ea2955a5a04
Devere Weaver <devere@deveres-mini.mynetworksettings.com>
2024-04-12 22:29:37 -0400

    implemented the ability for students to enroll as well as getting the roster for each course.

--- Assignment2/contracts/CSEnrollment.sol
+++ Assignment2/contracts/CSEnrollment.sol
@@ -15,16 +15,18 @@ contract CSEnrollment {
         Graduate
     }
 
-    struct Student {
-        address studentId;
-        uint8 numCredits;
-        Level studentLevel;
-    }
+    // Student not strictly necessary but may be useful later, just delete or 
+    // keep commented out until/if the time comes
+    // struct Student {
+    //     address studentId;
+    //     uint8 numCredits;
+    //     Level studentLevel;
+    // }
 
     struct Course {
         // string courseName;
         string courseNumber;
-        // bytes32 courseId;    // do I need a unique idenfitier like this?
+        // bytes32 courseId;    // does it need unique idenfitier like this?
         Level courseLevel;
         uint8 numCredits;
         uint8 capacity;
@@ -33,20 +35,17 @@ contract CSEnrollment {
     }
 
     Course[] courses;
+    // Student[] students;
 
 //==============================================================================
 // ESSENTIAL FUNCTIONS
 //==============================================================================
     constructor() {
         owner = msg.sender;
-
-        // console.log("Owner: %s", owner);
         addCourse("670", Level.Graduate);
         addCourse("617", Level.Graduate);
         addCourse("484", Level.Undergradute);
         addCourse("431", Level.Undergradute);
-        // console.log("Number of courses: %s", getCoursesLength());
-        printCourses();
     }
 
     modifier onlyOwner() {
@@ -58,7 +57,7 @@ contract CSEnrollment {
     function addCourse( string memory _courseNumber, Level _courseType) public onlyOwner {
         /** Adds a new course to the enrollment contract */
 
-        // Case: Empty courses array, just add the course 
+        // empty courses array => just add the course 
         if (courses.length == 0) {
             courses.push(Course({
                 courseNumber: _courseNumber,
@@ -72,7 +71,7 @@ contract CSEnrollment {
             return;
         }
 
-        // Handle all other cases
+        // all other cases
         for (uint i = 0; i < courses.length; i++) {
             if (keccak256(abi.encodePacked(courses[i].courseNumber)) == keccak256(abi.encodePacked(_courseNumber))) {
                 continue;
@@ -86,7 +85,7 @@ contract CSEnrollment {
                     courseRoster: new address[](0)
                 }));
                 // console.log(msg.sender);
-                return;    // add course and bail out 
+                return;   
             }
         }
     }
@@ -96,20 +95,40 @@ contract CSEnrollment {
     function register(uint8 credits, Level studentType, string memory course) public {
         /** Allow a student to register for an offered course. */
 
-        // TODO: Implement the code to create a student based on the credits and studentType arguments
-        Student memory student = Student({
-            studentId: msg.sender,
-            numCredits: credits,
-            studentLevel: studentType
-        });
-
-        // when a student EOA invokes this function 
         for (uint i = 0; i < courses.length; i++) {
             bool sameString = compareStrings(course, courses[i].courseNumber);
             
             if(sameString) {    // add it to the course roster
-                courses[i].courseRoster.push(student.studentId);
+                courses[i].courseRoster.push(msg.sender);
+                console.log("%s successfully registered for %s!", msg.sender, course);
+                return;
             }
+        }
+        console.log("ERROR: %s not registered for %s!", msg.sender, course);
+    }
+
+    function getRoster(string memory course) public view {
+        /** For now, simply prints the roster for a given course? */
+    
+        // find index of course
+        int index = -1;
+        for (uint i = 0; i < courses.length; i++) {
+            bool foundIndex = compareStrings(course, courses[i].courseNumber);
+            if (foundIndex == true) {
+                index = int(i);
+            } 
+        }
+
+        // course not found => bail out
+        if (index == -1) {
+            console.log("ERROR: %s not offered!", course);
+            return;
+        } 
+
+        // course found => return EOAs of registered students
+         console.log("Roster for %s:", course);
+         for (uint i = 0; i < courses[uint256(index)].courseRoster.length; i++) {
+            console.log("%s. %s", (i+1), courses[uint256(index)].courseRoster[i]);
         }
     }
 
@@ -117,6 +136,7 @@ contract CSEnrollment {
 // HELPER FUNCTIONS
 //==============================================================================
     function getCoursesLength() public view returns(uint) {
+        /** Get length of the courses array */
         return courses.length;
     }
 
@@ -127,7 +147,8 @@ contract CSEnrollment {
         }
     }
 
-    function compareStrings(string memory _a, string memory _b) private returns(bool) {
+    function compareStrings(string memory _a, string memory _b) private pure returns(bool) {
+        /** Compare string values...why isn't this built into Solidity? 🤔 */
         if (keccak256(abi.encodePacked(_a)) == keccak256(abi.encodePacked(_b))) {
             return true;
         } else {
@@ -141,7 +162,7 @@ contract CSEnrollment {
     }
 
     function tipOwner() public payable {
-        /** TU students, send me 💰! Pls n thx */
+        /** TU students, send me 💰, if you'd like! Pls n thx. */
         (bool success, ) = owner.call{value: msg.value}("");
         require(success);
     }
--- Assignment2/test/CSEnrollment.js
+++ Assignment2/test/CSEnrollment.js
@@ -1,8 +1,30 @@
-require("@nomicfoundation/hardhat-toolbox/network-helpers");
 const { expect } = require("chai");
 
-describe("Courses Offered:", function () {
-    it("Adding courses", async function () {
-        const cosc670 = await ethers.deployContract("CSEnrollment");
+describe("Testing CSEnrollment:", function () {
+    it("Adding courses", async () => {
+        const registrar = await ethers.deployContract("CSEnrollment");
+    });
+
+    it("Student registration", async () => {
+        // deploy contract
+        const registrar = await ethers.deployContract("CSEnrollment");
+
+        // Get different addresses to test with
+        const [owner, student1, student2, student3, student4] = await ethers.getSigners();
+
+        // Register the students for 670
+        await registrar.connect(student1).register(20, 0, '670');
+        await registrar.connect(student2).register(20, 0, '670');
+        await registrar.connect(student3).register(20, 0, '670');
+        await registrar.connect(student4).register(20, 0, '670');
+
+        // Let's check the four course rosters for accuracy 
+        console.log();
+        await registrar.getRoster('670');
+
+        // Register for nonexisting course 
+        await registrar.connect(student1).register(20, 0, '880');
+        console.log()
+        await registrar.getRoster('880');
     });
 });
